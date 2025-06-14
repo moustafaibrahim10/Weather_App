a5320468145b560813ce7eb5e0e3998bd99e967f
moustafaibrahim10 <mustafa444.ibrahim555@gmail.com>
2025-06-15 02:57:13 +0300

    first commit

---
+++ lib/core/utils/constances.dart
@@ -0,0 +1,3 @@
+class AppConstant{
+  static const String baseUrl = "https://api.openweathermap.org/data/2.5";
+}
---
+++ lib/domain/entities/weather.dart
@@ -0,0 +1,10 @@
+class Weather{
+  final int? id;
+  final String? main;
+  final String? description;
+  final String? icon;
+
+
+
+  Weather({this.id,this.main,this.description,this.icon});
+}
